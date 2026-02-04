334cf0efd84caaad24a494435f5851d97c235cba
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 22:03:18 +0800

    pass the slug value from views.py to post template html

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name="post")
+    path('', views.post_list, name="post"),
+    path('<slug:slug>', views.post_page, name="page"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    return HttpResponse(slug)
+
