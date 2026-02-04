c9a10bdc6cf05090391bb68a2319b7ff5f6e5684
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 22:11:10 +0800

    change links

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.post_list, name="post"),
+    path('', views.post_list, name="list"),
     path('<slug:slug>', views.post_page, name="page"),
 ]
