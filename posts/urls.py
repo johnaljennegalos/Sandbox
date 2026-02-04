1a26e5277bfbd9698290d529971283d44d055835
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 22:08:34 +0800

    designate the url to posts

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.post_list, name="post"),
     path('<slug:slug>', views.post_page, name="page"),
