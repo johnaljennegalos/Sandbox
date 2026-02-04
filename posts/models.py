7e634454d58a64ffa60952ce8b78d7f5ac755e70
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 20:10:43 +0800

    created a function in model to convert object to string

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
