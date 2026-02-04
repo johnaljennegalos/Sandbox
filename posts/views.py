14d7dc4c04459a94b9657937de1f9766f8c508c6
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 21:05:35 +0800

    display the list by latest

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
