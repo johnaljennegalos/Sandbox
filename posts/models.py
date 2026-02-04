16b59c7b7de4ef85b482447bcbc0023cc72293d9
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 19:45:17 +0800

    created a Post class  model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    slug = models.SlugField(max_length=200)
