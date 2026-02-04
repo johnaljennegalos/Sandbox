4fe110f0566b738f902d5c3f3cf63a62441baf45
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-04 19:48:40 +0800

    created a Post class  model

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,4 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     slug = models.SlugField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
