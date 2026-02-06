2d25c637fe3459fff14de36a0af3bf618926e8a1
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-06 23:22:40 +0800

    added a new post a tag and import the decorators

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
@@ -10,6 +11,9 @@ def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', {'post': post})
 
+@login_required(login_url='/users/login/')
+def new_post(request):
+    if request.method == 'POST':
 def posts_new(request):
     return render(request, 'posts/posts_new.html')
 
