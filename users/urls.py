717cf9e327d18985d86a8fdc2127a3fa8d6a9554
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-06 22:47:09 +0800

    created a logout path in urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.user_register, name="user_register"),
-    path('login/', views.login_view, name="login")
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 ]
