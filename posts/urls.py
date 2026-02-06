2a08282861e6ee1727284a6e06f96c7ff4ef74a8
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-06 23:11:20 +0800

    added a new path for new post

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.post_list, name="list"),
+    path('new/post', views.posts_new, name="new_post"),
     path('<slug:slug>', views.post_page, name="page"),
 ]
