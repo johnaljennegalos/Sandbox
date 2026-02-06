6d88501d78cfd29d017556e40886bf49ddf3f1e5
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-06 22:11:30 +0800

    import the login in views and change login logic

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -7,7 +8,7 @@ def user_register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect('posts:list')
     else:
         form = UserCreationForm()
@@ -17,7 +18,7 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            #Login
+            login(request, form.get_user())
             return redirect('posts:list')
     else:
         form = AuthenticationForm()
